f4cd0521cef683d9bcc95fef5466197d318426de
Oliver Dorrian <dorrianolfc@gmail.com>
2022-05-11 20:38:46 +0100

    Changes to Tutor settings

--- settings.js
+++ settings.js
@@ -1,6 +1,7 @@
 var account = document.getElementById("account");
 var courses = document.getElementById("courses");
 var password = document.getElementById("password");
+var content = document.getElementById("content");
 var bugs = document.getElementById("bugs");
 
 function disableBodyScroll(){
@@ -17,8 +18,8 @@ document.getElementById("accountClick").addEventListener("click", function(){
     password.style.visibility = "hidden";
     password.style.position = "absolute";
 
-    bugs.style.visibility = "hidden";
-    bugs.style.position = "absolute";
+    content.style.position = "hidden";
+    content.style.visibility = "absolute";
 
     account.style.position = "relative";
     account.style.visibility = "visible";
@@ -29,12 +30,31 @@ document.getElementById("courseClick").addEventListener("click", function(){
     account.style.position = "absolute";
     password.style.visibility = "hidden";
     password.style.position = "absolute";
-    bugs.style.visibility = "hidden";
-    bugs.style.position = "absolute";
+    content.style.position = "hidden";
+    content.style.visibility = "absolute";
 
     courses.style.position = "relative"
     courses.style.visibility = "visible";
 });
+
+// content appear
+document.getElementById("contentClick").addEventListener("click", function(){
+    account.style.visibility = "hidden";
+    account.style.position = "absolute";
+
+    bugs.style.visibility = "hidden";
+    bugs.style.position = "absolute";
+
+    courses.style.visibility = "hidden";
+    courses.style.position = "absolute";
+
+    password.style.visibility = "hidden";
+    password.style.position = "absolute";
+
+    content.style.position = "relative";
+    content.style.visibility = "visible";
+});
+
 
 // password appear
 document.getElementById("passwordClick").addEventListener("click", function(){
@@ -46,6 +66,9 @@ document.getElementById("passwordClick").addEventListener("click", function(){
 
     courses.style.visibility = "hidden";
     courses.style.position = "absolute";
+    
+    content.style.visibility = "absolute";
+    content.style.position = "hidden";
 
     password.style.position = "relative"
     password.style.visibility = "visible";
--- tutorHome.php
+++ tutorHome.php
@@ -19,10 +19,10 @@
         <link rel="stylesheet" href="settings.css">
         <link rel="icon" href="assets/favicon.svg">
     </head>
-    <body>
+    <body id="appBody">
         <div id="sideBar">
-            <div id="sideBarLogo">
-                <p>Ace <b>Academy</b></p>
+            <div id="titleSet">
+                <h6>Settings ⚙️</h6>
             </div>
             <div class="course">
                 <div class="courseVis">
@@ -31,32 +31,17 @@
             </div>
             <div class="course">
                 <div class="courseVis">
-                    <p class="courseText" id="courseClick">Courses</p>
+                    <p class="courseText" id="courseClick">Files</p>
                 </div>
             </div>
             <div class="course">
+                <div class="courseVis">
+                    <p class="courseText" id="contentClick">Content</p>
+                </div>
+            </div>
+            <div id="lastList" class="course">
                 <div class="courseVis">
                     <p class="courseText" id="passwordClick">Password</p>
-                </div>
-            </div>
-            <div class="course">
-                <div class="courseVis">
-                    <p class="courseText" id="bugClick">Bug reports</p>
-                </div>
-            </div>
-            <div class="course">
-                <div class="courseVis">
-                    <p class="courseText">Raw Data</p>
-                </div>
-            </div>
-            <div class="course">
-                <div class="courseVis">
-                    <p class="courseText">Tutors</p>
-                </div>
-            </div>
-            <div class="course">
-                <div class="courseVis">
-                    <p class="courseText">Contact</p>
                 </div>
             </div>
         </div>
@@ -66,10 +51,10 @@
             </div>
             <div class="main">
             <form id="formBox" action="includes/changeName.inc.php" method="post">
-                <h1>Change name</h1>
-                <h2 class="Lable">Forename</h2>
+                <h2>Change name</h2>
+                <h3 class="Lable">Forename</h3>
                 <input class="inputThing" type="text" name="name"></input>
-                <h2 class="Lable">Surname</h2>
+                <h3 class="Lable">Surname</h3>
                 <input class="inputThing" type="text" name="sName"></input>
                 <input class="buttonPress" type="submit" value="Login" name="submit"></input>
             </form>
@@ -77,65 +62,69 @@
         </div>
         <div class="mainContent" id="courses">
             <div class="titleBit blob">
-                <p><b>Courses</b></p>
+                <p><b>Upload Files</b></p>
             </div>
             <div class="main">
-            <?php uploadResource($conn);?>
+                <?php uploadResource($conn);?>
 
-            <form method="post" action="" enctype="multipart/form-data">
-                <label for="uploadFile">File to Upload</label>
-                <input type="file" name="uploadFile"/>
-                <br/>
-                <input type="radio" id="webdev" name="courseId" value="1">
-                <label for="webdev">Web Dev</label><br>
-                <input type="radio" id="psych" name="courseId" value="2">
-                <label for="psych">Psychology</label><br>
-                <input type="radio" id="maths" name="courseId" value="3">
-                <label for="maths">Maths</label><br>
-                <input type="radio" id="banking" name="courseId" value="4">
-                <label for="banking">Banking</label><br>
-                <input type="submit" value="UPLOAD FILE"/>
-            </form>
-
-            <br/>
-            <br/>
-
-            <?php uploadPost($conn);?>
-            <form method="post" action="" enctype="multipart/form-data">
-                <label for="title">Title: </label>
-                <input type="text" name="title"></input>
-                <label for="uploadFile">Image to Upload</label>
-                <input type="file" name="uploadImg"/>
-                <br/>
-                <label for="content">Content: </label><br/>
-				<textarea name="content" cols="80" rows="10"></textarea>
-                <input type="radio" id="webdev" name="courseId" value="1">
-                <label for="webdev">Web Dev</label><br>
-                <input type="radio" id="psych" name="courseId" value="2">
-                <label for="psych">Psychology</label><br>
-                <input type="radio" id="maths" name="courseId" value="3">
-                <label for="maths">Maths</label><br>
-                <input type="radio" id="banking" name="courseId" value="4">
-                <label for="banking">Banking</label><br>
-                <input type="submit" value="UPLOAD POST"/>
-            </form>
-            <?php authoriseStudents($conn); ?>
-            <?php authoriseStudentsForm($conn); ?>
-            <a href="quizstart.php">Click here to create a quiz!</a>
+                <form method="post" action="" enctype="multipart/form-data">
+                    <label for="uploadFile">File to Upload</label>
+                    <input type="file" name="uploadFile"/>
+                    <input type="radio" id="webdev" name="courseId" value="1">
+                    <label for="webdev">Web Dev</label><br>
+                    <input type="radio" id="psych" name="courseId" value="2">
+                    <label for="psych">Psychology</label><br>
+                    <input type="radio" id="maths" name="courseId" value="3">
+                    <label for="maths">Maths</label><br>
+                    <input type="radio" id="banking" name="courseId" value="4">
+                    <label for="banking">Banking</label>
+                    <br>
+                    <input type="submit" value="UPLOAD FILE"/>
+                </form>
             </div> 
         </div>
+
+        <div class="mainContent" id="content">
+            <div class="titleBit blob">
+                <p><b>Add Content</b></p>
+            </div>
+            <div class="main">
+                <?php uploadPost($conn);?>
+                <form method="post" action="" enctype="multipart/form-data">
+                    <label for="title">Title: </label>
+                    <input type="text" name="title"></input>
+                    <label for="uploadFile">Image to Upload</label>
+                    <input type="file" name="uploadImg"/>
+                    <br/>
+                    <label for="content">Content: </label><br/>
+				    <textarea name="content" cols="80" rows="10"></textarea>
+                    <input type="radio" id="webdev" name="courseId" value="1">
+                    <label for="webdev">Web Dev</label><br>
+                    <input type="radio" id="psych" name="courseId" value="2">
+                    <label for="psych">Psychology</label><br>
+                    <input type="radio" id="maths" name="courseId" value="3">
+                    <label for="maths">Maths</label><br>
+                    <input type="radio" id="banking" name="courseId" value="4">
+                    <label for="banking">Banking</label><br>
+                    <input type="submit" value="UPLOAD POST"/>
+                </form>
+                <?php authoriseStudents($conn); ?>
+                <?php authoriseStudentsForm($conn); ?>
+                <a href="quizstart.php">Click here to create a quiz!</a>
+            </div> 
+        </div>
+
         <div class="mainContent" id="password">
             <div class="titleBit blob">
-                <p><b>Password</b></p>
+                <p><b>Change Password</b></p>
             </div>
             <div class="main">
             <form id="formBox" action="includes/changePass.inc.php" method="post">
-                    <h1>Change Password</h1>
-                    <h2 class="Lable">Current Password</h2>
+                    <h3 class="Lable">Current Password</h3>
                     <input class="inputThing" type="text" name="curPwd"></input>
-                    <h2 class="Lable">New Password</h2>
+                    <h3 class="Lable">New Password</h3>
                     <input class="inputThing" type="text" name="newPwd"></input>
-                    <h2 class="Lable">New Password Repeat</h2>
+                    <h3 class="Lable">New Password Repeat</h3>
                     <input class="inputThing" type="text" name="newPwdR"></input>
                     <input class="buttonPress" type="submit" value="Login" name="submit"></input>
                     </form>
